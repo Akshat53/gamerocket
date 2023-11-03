a2d90470c12821cb408f3f9f2438538161fb526b
Akshat singh <singh.akshat.akshat26@gmail.com>
2023-11-03 18:51:38 +0530

    gamerocket

---
+++ src/config/connectDB.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: '213.239.217.56',
+    user: 'gamerock_jhnibvcfgddfv',
+    password: 'bhgh5345sdfdf',
+    database: 'gamerock_jhnibvcfgddfv'
+});
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'gamerock_jhnibvcfgddfv'
+// });
+   // "start": "pm2 start src/server.js"
+
+export default connection;
