09c4405ab7724637e79dbac5ca5576244b91caf6
Akshat singh <singh.akshat.akshat26@gmail.com>
2023-11-07 14:22:25 +0530

    db upadted

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
-    host: '213.239.217.56',
-    user: 'gamerock_jhnibvcfgddfv',
-    password: 'bhgh5345sdfdf',
-    database: 'gamerock_jhnibvcfgddfv'
-});
+// const connection = mysql.createPool({
+//     host: '213.239.217.56',
+//     user: 'gamerock_jhnibvcfgddfv',
+//     password: 'bhgh5345sdfdf',
+//     database: 'gamerock_jhnibvcfgddfv'
+// });
 
 // const connection = mysql.createPool({
 //     host: 'localhost',
@@ -13,6 +13,13 @@ const connection = mysql.createPool({
 //     password: '',
 //     database: 'gamerock_jhnibvcfgddfv'
 // });
+
+const connection = mysql.createPool({
+    host: '213.239.217.56',
+    user: 'gamerock_jhnibvcfgddfv',
+    password: 'bhgh5345sdfdf',
+    database: 'gamerock_Akshat'
+});
    // "start": "pm2 start src/server.js"
 
 export default connection; 
